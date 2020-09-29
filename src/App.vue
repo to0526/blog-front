402758d5a15bb2e365f2158e2837e5a9c6c5924d
Ohtsuka Takahiro <t.o.0526@gmail.com>
2020-09-29 14:28:55 +0900

    ナビゲーションバー

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div id="app">
+    <b-navbar variant="light">
+      <b-navbar-brand>
+        Takalog
+      </b-navbar-brand>
+      <b-navbar-nav>
+        <b-navbar-item class="mr-2"><router-link to="/">Home</router-link></b-navbar-item>
+        <b-navbar-item class="mr-2"><router-link to="/about">About</router-link></b-navbar-item>
+        <b-navbar-item class="mr-2"><router-link to="/articles">ブログ一覧</router-link></b-navbar-item>
+      </b-navbar-nav>
+    </b-navbar>
     <b-container>
-      <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link> |
-        <router-link to="/articles">ブログ一覧</router-link>
-      </div>
       <router-view/>
     </b-container>
   </div>
 </template>
 
 <style>
+a {
+  color: black;
+}
 </style>

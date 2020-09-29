0ce2a4c799d4e43ed396fcc06f0eb5bcd87e16b0
Ohtsuka Takahiro <t.o.0526@gmail.com>
2020-09-29 11:46:57 +0900

    ブログ一覧取得

--- src/views/ArticleList.vue
+++ src/views/ArticleList.vue
@@ -1,5 +1,38 @@
 <template>
-  <h1>
-    ブログ記事一覧
-  </h1>
+  <div>
+    <h1>
+      ブログ記事一覧
+    </h1>
+    <ul v-for="article in articles" v-bind:key="article.id">
+      <li>
+        タイトル: {{article.title}}
+        本文: {{article.body}}
+        作成日時: {{article.created_at}}
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      articles: []
+    }
+  },
+  methods: {
+    fetchArticles: function() {
+      fetch('http://localhost:3000/articles')
+        .then(response => response.json())
+        .then(data => this.articles = data)
+        .catch(error => alert(error))
+    }
+  },
+  created: function() {
+    this.fetchArticles()
+  }
+}
+</script>
+
+<style>
+</style>

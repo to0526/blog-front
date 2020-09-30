eefd52496611c882707690bab39b07559d96673d
Ohtsuka Takahiro <t.o.0526@gmail.com>
2020-09-30 09:32:11 +0900

    ログインページ作成

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,9 @@
         <b-nav-item class="mr-2">
           <router-link to="/about">About</router-link>
         </b-nav-item>
+        <b-nav-item class="mr-2">
+          <router-link to="/sign_in">SignIn</router-link>
+        </b-nav-item>
       </b-navbar-nav>
     </b-navbar>
     <b-container>
---
+++ src/views/SignIn.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2 class="my-4">
+      ログイン
+    </h2>
+    <b-form @submit="onSubmit">
+      <b-form-group id="email" label="Email address" label-for="email">
+        <b-form-input id="email" v-model="email" type="email" required>
+        </b-form-input>
+      </b-form-group>
+
+      <b-form-group id="password" label="password" label-for="password">
+        <b-form-input id="password" v-model="password" type="password" required>
+        </b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: ""
+    }
+  },
+  methods: {
+    onSubmit: function(event) {
+      event.preventDefault()
+      alert(this.email)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
